1531b864c25ae7db1ba840cfbcf47768558bd1f9
darkLord <ismoiljoraxonov1@gmail.com>
2026-05-01 10:06:05 +0900

    add

--- src/main/java/dev/ismoil/chat_service/config/WebSocketConfig.java
+++ src/main/java/dev/ismoil/chat_service/config/WebSocketConfig.java
@@ -1,4 +1,19 @@
-package dev.ismoil.chat_service;
+package dev.ismoil.chat_service.config;
 
-public class WebSocketConfig {
+import dev.ismoil.chat_service.handler.WebSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+public class WebSocketConfig implements WebSocketConfigurer {
+
+
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+
+        registry.addHandler(new WebSocketHandler(), "ws/chat");
+
+    }
 }
